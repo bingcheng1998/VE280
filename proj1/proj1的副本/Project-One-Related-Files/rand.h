57a362a4574b54d3025eb8da86246ff784c17209
Bingcheng <bingcheng1998@qq.com>
2018-11-21 15:27:20 +0800

    删除旧文件

--- proj1/proj1的副本/Project-One-Related-Files/rand.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RAND_H__
-#define __RAND_H__
-
-void p1_srand(int);
-// EFFECTS: initializes the seed
-int p1_rand();
-// EFFECTS: returns a random number between 13 and 39
-
-#endif /* __RAND_H__ */
